0db0a779b846c8d98260e1df0a7f701b6b46ebab
Aldito8 <alditositompul198@gmail.com>
2025-11-07 05:32:07 +0700

    feat/post: condition no data feed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { formatDistanceToNow } from "date-fns"
 import { api } from "@/utils/axios"
 import { useEffect, useState } from "react"
-import { Textarea } from "@/components/ui/textarea"
 
 interface FeedItem {
     id: number
@@ -28,14 +27,13 @@ export default function HomePage() {
             <div className="w-full flex-col justify-center p-4">
                 <h1 className="text-2xl font-bold mb-4 text-center">Feed</h1>
                 <div className="space-y-4">
-                    {data.map((item) => (
+                    {data.length > 0 ? data.map((item) => (
                         <div
                             key={item.id}
                             className="bg-white shadow-md rounded-2xl p-4 border border-gray-200 hover:shadow-lg transition"
                         >
                             <div className="flex justify-between items-center mb-2">
-                                <span>id: {item.id}</span>
-                                <span className="text-sm text-gray-600">
+                                <span className="text-sm text-gray">
                                     User ID: {item.user_id}
                                 </span>
                                 <span className="text-xs text-gray-400">
@@ -44,7 +42,9 @@ export default function HomePage() {
                             </div>
                             <p className="text-gray-800 wrap-break-word">{item.content}</p>
                         </div>
-                    ))}
+                    )) : (
+                        <p className="text-center">Looks like your feed is quiet. Follow more people to liven it up!</p>
+                    )}
                 </div>
             </div>
         </div>
